3848fb6bf90655df06e4560648b76f33dd464620
ViniciusCamargo1 <vinidilelio@gmail.com>
2026-03-05 15:54:21 -0300

    Atividades Variáveis

---
+++ ativiadesVariaveis.py
@@ -0,0 +1,47 @@
+resultado = (4 * 6)/2
+print("o resultado é :", resultado)
+
+soma = (5 + 7)
+print( 'resultado:', soma)
+
+potencia = (3 ** 4)
+print ('potencia', resultado)
+
+média = (8 + 12 + 15)/3
+print ('O resultado da média é :', média)
+
+subtração = (10 - 2)*5 
+print ("O resultado da subtração é", subtração)
+
+div = ( 27 / 3 ) + 5
+print ( "O resultado da divisão é:", div)
+
+módulo = ( 17 % 4) 
+print ( "Resultado", módulo )
+
+multi = ( 9 * 3) **1
+print ("Resultado :", multi)
+
+raiz = 81**0.5
+print ("Resultado", raiz)
+
+adicione = ( 3 * 4) + 20
+print ("Resultado", adicione)
+
+multiplicação = (15 * 2 ) -7
+print ("Resultado", multiplicação)
+
+cubo = ( 5 **3)
+print ("Resultado", cubo)
+
+média2 = ( 17 + 21 + 25)/3
+print ("Resultado", média2)
+
+multi2 = ( 11  * 2) + 7
+print ("Resultado", multi2)
+
+sub = ( 3 * 8 - 15) / 2
+print ("Resultado", sub) 
+
+potencia2 = ( 2 ** 10)
+print ("Resultado", potencia2)
