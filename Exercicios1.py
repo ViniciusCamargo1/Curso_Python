98e72a7c97a8b3614081fdf5a43b80c9b0f72641
ViniciusCamargo1 <vinidilelio@gmail.com>
2026-03-11 17:57:16 -0300

    Add files via upload

---
+++ Exercicios1.py
@@ -0,0 +1,25 @@
+print("exercicio 1")
+numero = int(input('Digite 1 numero:'))
+if numero > 0:
+   print ("O numero é positivo")
+elif numero == 0:
+   print ("o numero é igual a zero")
+elif numero < 0:
+   print ("o numero é negativo")
+
+   print ("exercicio 2")
+idade = int(input("Digite a sua idade:"))
+if idade >= 16:
+   print ("pode votar")
+else:
+   print ("Não pode votar")
+   
+   print ("exercicio 3")
+numero_aleatorio = 15
+if numero_aleatorio % 2 == 0:
+   print ("o Numero é par")
+else: 
+   print ("o Numero é impar")
+   
+   print( "exercicio 4")
+
---
+++ Reservas_Hotel.py
@@ -0,0 +1,26 @@
+print('Bem vindo ao Hotel')
+numero_clientes  = list(range(1,3))
+quantos_clientes = int(input('Qual a quantidade de clientes ?'))
+
+cliente_01 = (input('Digite seu nome:'))
+cliente_01 = int(input('Digite sua idade:'))
+
+cliente_02 = (input('Digite seu nome:'))
+cliente_02 = int(input('Digite sua idade:'))
+
+cliente_03 = (input('Digite seu nome:'))
+cliente_03 = int(input('Digite sua idade:'))
+
+simples = "R$100"
+duplo = "R$150"
+luxo = "R$250"
+
+lista_diaria = (simples,duplo,luxo)
+lista_cliente01 = (input("Digite qual será o seu quarto:"))
+lista_cliente02 = (input("Digite qual será o seu quarto:"))
+lista_cliente03 = (input("Digite qual será o seu quarto:"))
+
+dias = (input("Quantos dias de reserva?"))
+Valor_Total = (lista_cliente01)
+
+
