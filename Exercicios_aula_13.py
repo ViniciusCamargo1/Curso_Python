c7369c5b23c1a1fd43fabb932dea28a66f81599e
ViniciusCamargo1 <vinidilelio@gmail.com>
2026-03-26 17:55:43 -0300

    Terminar

---
+++ Exercicios_aula_13.py
@@ -0,0 +1,20 @@
+import random
+
+print("Exercicio 1:")
+numero_aleatorio = random.randint(5,10)
+print("O numero aleatorio é:", numero_aleatorio)
+
+print("Exercicio 2:")
+numero_aleatorio1 = random.randint(1,99)
+numero_aleatorio2 = random.randint(1,99)
+numero_aleatorio3 = random.randint(1,99)
+print("O numero 1 aleatorio é:",numero_aleatorio1)
+print("O numero 2 aleatorio é:",numero_aleatorio2)
+print("O numero 3 aleatorio é:",numero_aleatorio3)
+
+print("Exercicio 3")
+numero_range = random.randint in range (10,30)
+print(numero_range) 
+
+print ("Exercicio 4")
+contagem_simples = 10
