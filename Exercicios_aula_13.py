b935409756def9c29fc57e3eae58d5413e8fed1c
ViniciusCamargo1 <vinidilelio@gmail.com>
2026-04-09 16:35:19 -0300

    Update Exercicios_aula_13.py

--- Exercicios_aula_13.py
+++ Exercicios_aula_13.py
@@ -1,20 +1,57 @@
 import random
+print ("Atividade 1")
 
-print("Exercicio 1:")
-numero_aleatorio = random.randint(5,10)
-print("O numero aleatorio é:", numero_aleatorio)
+def atividade1():
+    x = random.randint(5, 10)
+    return x
+  
+print ("Atividade 2")
+def atividade2():
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    z = random.randint(1, 10)
+    return x, y, z
 
-print("Exercicio 2:")
-numero_aleatorio1 = random.randint(1,99)
-numero_aleatorio2 = random.randint(1,99)
-numero_aleatorio3 = random.randint(1,99)
-print("O numero 1 aleatorio é:",numero_aleatorio1)
-print("O numero 2 aleatorio é:",numero_aleatorio2)
-print("O numero 3 aleatorio é:",numero_aleatorio3)
+print ("Atividade 3")
+def atividade3():
+    x = random.choice(range(10, 31)) 
+    return x
+print ("Atividade 4")
+def atividade4():
+    for i in range(10, 0, -1):
+        print(i)
+    print("Fogo!")
 
-print("Exercicio 3")
-numero_range = random.randint in range (10,30)
-print(numero_range) 
-
-print ("Exercicio 4")
-contagem_simples = 10
+print ("Atividade 5")
+def atividade5():
+    numero_str = input("Insira um número inteiro positivo: ")
+    
+    if numero_str.isdigit():
+        numero = int(numero_str)
+        soma = 0
+        
+        for i in range(2, numero + 1):
+            if i % 2 == 0: 
+                soma += i
+                
+        return f"A soma dos números pares de 2 até {numero} é: {soma}"
+    else:
+        return "Entrada inválida. Por favor, insira apenas números inteiros."
+      
+print ("Atividade 6")
+def atividade6():
+    numero_str = input("Insira um número inteiro para ver a tabuada: ")
+    
+    try:
+        numero = int(numero_str)
+        print(f"\n--- Tabuada do {numero} ---")
+        for i in range(1, 11):
+            print(f"{numero} x {i} = {numero * i}")
+    except ValueError:
+        print("Entrada inválida. Você não digitou um número inteiro.")
+      
+print ("Atividade 7")
+def atividade7():
+    for i in range(99, 0, -2):
+        print(i, end=" ")
+    print()
